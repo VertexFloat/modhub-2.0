1979a4bf31e052f1dfc002f5d0c92c049b2dfe3a
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-03-06 13:22:32 +0100

    removed unnecessary files

--- src/js/popup.js
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 4c65736975. All rights reserved.
-
-const githubBtn = document.getElementById('button-redirect-github')
-
-githubBtn.addEventListener('click', () => {
-    const url = 'https://github.com/4c65736975/Modhub-2.0'
-
-    return chrome.tabs.create({url: url})
-})
