f7d50aa649c2c7bb5145dfdd6701ec1f6fb160a8
4c65736975 <107006334+4c65736975@users.noreply.github.com>
2024-03-08 13:31:05 +0100

    created test app

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Loading from "./components/Loading.vue";
+
+const isLoading = ref(true)
+
+window.addEventListener("message", (event) => {
+  if (event.data && event.data.message === "scraped_data") {
+    setTimeout(() => {
+      isLoading.value = false
+    }, 250)
+  }
+})
+</script>
+
+<template>
+  <Header>
+    <div></div>
+  </Header>
+  <Loading v-if="isLoading"/>
+  <Footer/>
+</template>
+
+<style scoped>
+</style>
